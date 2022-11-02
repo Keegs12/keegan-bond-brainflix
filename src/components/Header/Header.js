27d258e9c3a6546a0058fe938cd80b7c949702a8
Keegan Bond <keeganbond123@gmail.com>
2022-11-02 00:07:59 -0400

    create header component, styled the mobile design and began working on tablet/desktop, header component has all features, further styling and possible functionality required

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import BrainFlixLogo from "../../assets/images/Logo/BrainFlix-logo.svg";
+import Avatar from "../../assets/images/Images/Mohan-muruge.jpg";
+import "./Header.scss";
+
+function Header(props) {
+    return (
+        <header className="Header">
+            {/* <div className="Header__container"> */}
+            <img
+                className="Header__logo"
+                src={BrainFlixLogo}
+                alt="BrainFlixLogo, Blue Youtube"
+            ></img>
+            <label
+                className="Header__search-bar-input"
+                htmlFor="search"
+            ></label>
+            <input
+                className="Header__search-bar"
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search"
+            ></input>
+            <button className="Header__button">UPLOAD</button>
+            <img
+                className="Header__avatar"
+                src={Avatar}
+                alt="BrainFlix Avatar Icon"
+            ></img>
+            {/* </div> */}
+        </header>
+    );
+}
+
+export default Header;
